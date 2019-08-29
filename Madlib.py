86993082620f0ef07f125a05ca29e586847e0d57
Snixerz <cakeisew@gmail.com>
2019-08-29 14:38:05 -0700

    Create Madlib.py

---
+++ Madlib.py
@@ -0,0 +1,7 @@
+print("The {noun} is very {adjective} and is going to {verb}")
+
+noun = input("Enter a noun: ")
+adjective = input("Enter an adjective: ")
+verb = input("enter a verb: ")
+
+print("The " + noun + " is very " + adjective + " and is going to " + verb)
