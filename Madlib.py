e53d9912dcd6973ba730ceea21c56caa985235a8
Snixerz <cakeisew@gmail.com>
2019-08-29 15:25:00 -0700

    Added more stories

--- Madlib.py
+++ Madlib.py
@@ -1,4 +1,12 @@
+number = input("Select a random story from numbers 1-5: ")
 noun = input("Enter a noun: ")
 adjective = input("Enter an adjective: ")
 verb = input("enter a verb: ")
 
+
+
+print(f"The {noun} is very {adjective} and is going to {verb}")
+print(f"The {adjective} {noun} went to {verb}")
+print(f"At the top of the hill a {noun} was feeling {adjective} and decided to {verb}")
+print(f"Inside the car a {noun} was {adjective} and insisted to {verb}")
+print(f"After school the {noun} was {adjective} and decided to {verb}")
