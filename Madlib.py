0804c5415e95f55ef11fe0d8dfea2594708c8262
Snixerz <cakeisew@gmail.com>
2019-08-29 16:22:23 -0700

    Fixed a bug in the story

--- Madlib.py
+++ Madlib.py
@@ -4,9 +4,3 @@ adjective = input("Enter an adjective: ")
 verb = input("enter a verb: ")
 
 
-
-print(f"The {noun} is very {adjective} and is going to {verb}")
-print(f"The {adjective} {noun} went to {verb}")
-print(f"At the top of the hill a {noun} was feeling {adjective} and decided to {verb}")
-print(f"Inside the car a {noun} was {adjective} and insisted to {verb}")
-print(f"After school the {noun} was {adjective} and decided to {verb}")
