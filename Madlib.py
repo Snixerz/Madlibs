d7187aa7d669082d4330a1616f3d66f691b8f301
Snixerz <cakeisew@gmail.com>
2019-08-29 16:23:17 -0700

    Added if else statements #1-5

--- Madlib.py
+++ Madlib.py
@@ -3,4 +3,15 @@ noun = input("Enter a noun: ")
 adjective = input("Enter an adjective: ")
 verb = input("enter a verb: ")
 
-
+if number == "1":
+  print(f"The {noun} is very {adjective} and is going to {verb}")
+elif number == "2":
+  print(f"The {adjective} {noun} went to {verb}")
+elif number == "3":
+  print(f"At the top of the hill a {noun} was feeling {adjective} and decided to {verb}")
+elif number == "4":
+  print(f"Inside the car a {noun} was {adjective} and insisted to {verb}")
+elif number == "5":
+  print(f"After school the {noun} was {adjective} and decided to {verb}")
+else:
+  print("You did not select a correct number, try again.")
