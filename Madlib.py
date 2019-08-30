6ea4fc7063c67a5fa6082132c5663dd2f29feefd
Snixerz <cakeisew@gmail.com>
2019-08-30 12:53:10 -0700

    Added a dictionary

--- Madlib.py
+++ Madlib.py
@@ -1,4 +1,13 @@
+story_selector = {
+  "1": "The {noun} is very {adjective} and is going to {verb}",
+  "2": "The {adjective} {noun} went to {verb}",
+  "3": "At the top of the hill a {noun} was feeling {adjective} and decided to {verb}",
+  "4": "Inside the car a {noun} was {adjective} and insisted to {verb}",
+  "5": "After school the {noun} was {adjective} and decided to {verb}",
+}
+
 number = input("Select a random story from numbers 1-5: ")
+print(story_selector.get(number))
 noun = input("Enter a noun: ")
 adjective = input("Enter an adjective: ")
 verb = input("enter a verb: ")
